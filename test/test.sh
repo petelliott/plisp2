bbce3b359320017932ba36159e10a65ea11d973f
Peter Elliott <pelliott@ualberta.ca>
2020-04-28 19:43:54 -0600

    added travis

--- test/test.sh
+++ test/test.sh
@@ -2,6 +2,8 @@
 
 DIR=$1
 
+code=0
+
 for test in $(find $DIR -name '*.scm')
 do
     out="$(echo $test | cut -f 1 -d '.').out"
@@ -9,6 +11,9 @@ do
     then
         echo -e "\e[32;1mPASS\e[0m" $test
     else
+        code=1
         echo -e "\e[31;1mFAIL\e[0m" $test
     fi
 done
+
+exit $code
