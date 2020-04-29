867e29be9e0b6db9fa13f59fa46d4eb45b2de294
Peter Elliott <pelliott@ualberta.ca>
2020-04-28 19:50:45 -0600

    changed lightning prefix

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@ code=0
 for test in $(find $DIR -name '*.scm')
 do
     out="$(echo $test | cut -f 1 -d '.').out"
-    if PLISP_BOOT=scm/boot.scm ./plisp $test | diff - $out
+    if PLISP_BOOT=scm/boot.scm ./plisp $test | diff $out -
     then
         echo -e "\e[32;1mPASS\e[0m" $test
     else
